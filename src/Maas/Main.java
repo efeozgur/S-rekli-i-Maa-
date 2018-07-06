489e07f131cf4b2ca4f53dbb64db3b452440b6ee
Efe Özgür <efecuneytozgur@gmail.com>
2018-07-07 01:12:24 +0300

    Değiştir 1111

--- src/Maas/Main.java
+++ src/Maas/Main.java
@@ -19,6 +19,8 @@ public class Main {
        kesintiler.setDamgaVergisi(hakedis);
        kesintiler.setKesintilerToplami(sgk);
        hakedis.setToplamTahakkuk(sgk);       
+       
+       
        System.out.println(kisi.getAdSoyad()+" kişisine ait maaş dökümü");
        System.out.println("********************************************");       
        System.out.println("Günlük Ücret : " + hakedis.getGunlukUcret());
@@ -27,15 +29,19 @@ public class Main {
        System.out.println("Yakacak Yardımı : " + Sabitler.YAKACAK_YARDIM_UCRETİ);
        System.out.println("Bayram Yardımı : " + hakedis.getBayramYardimi());      
        System.out.println("Hakediş Toplamı : " + hakedis.hakedisToplami());       
-       System.out.println("***********************************************");      
+       System.out.println("***********************************************");   
+       
        System.out.println("SGK Matrahı : " + sgk.getSGK_Matrah());       
        System.out.println("%20.5 : " + sgk.getYuzdeYirmiBucuk());
        System.out.println("%14 : " + sgk.getYuzdeOnDort());
        System.out.println("%2 : " + sgk.getYuzdeIki());
        System.out.println("%1 : " + sgk.getYuzdeBir());      
        System.out.println("***********************************************");
+       
        System.out.println("Toplam Tahakkuk : " + hakedis.getToplamTahakkuk());
-       System.out.println("**************Kesintiler***********************");        
+       
+       System.out.println("**************Kesintiler***********************");   
+       
        System.out.println("Gelir Vergisi Matrahı : " + kesintiler.getGV_Matrahi());
        System.out.println("Asgari Geçim İndirimi Tutarı : " + kesintiler.getAsgariGecimIndirimi());       
        System.out.println("Gelir Vergisi  : " + kesintiler.getGelirVergisi());
