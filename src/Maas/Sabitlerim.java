6a01168c3239952aeeb9800746d8bf83621a1d76
Efe Özgür <efecuneytozgur@gmail.com>
2018-07-06 23:59:42 +0300

    Sabitleri Ekledik

---
+++ src/Maas/Sabitlerim.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Maas;
+
+/**
+ *
+ * @author Efe Özgür
+ */
+public enum Sabitlerim {
+   BEKAR,
+   EVLI_ESI_CALISMAYAN,
+   EVLI_ESI_CALISMAYAN_1_COCUK,
+   EVLI_ESI_CALISMAYAN_2_COCUK,
+   EVLI_ESI_CALISMAYAN_3_COCUK,
+   EVLI_ESI_CALISMAYAN_4_COCUK,
+   EVLI_ESI_CALISMAYAN_5_COCUK,
+   EVLI_ESI_CALISAN,
+   EVLI_ESI_CALISAN_1_COCUK,
+   EVLI_ESI_CALISAN_2_COCUK,
+   EVLI_ESI_CALISAN_3_COCUK,
+   EVLI_ESI_CALISAN_4_COCUK,
+   EVLI_ESI_CALISAN_5_COCUK,
+   
+    
+}
