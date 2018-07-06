a9ed6ec2dc57919b36a24e970587815095c2ba41
Efe Özgür <efecuneytozgur@gmail.com>
2018-07-07 01:04:56 +0300

    Değiştir

--- src/Maas/Main.java
+++ src/Maas/Main.java
@@ -30,16 +30,12 @@ public class Main {
        System.out.println("Bayram Yardımı : " + hakedis.getBayramYardimi());      
        System.out.println("Hakediş Toplamı : " + hakedis.hakedisToplami());
        
-       System.out.println("***********************************************");
-       
-       
+       System.out.println("***********************************************");      
        System.out.println("SGK Matrahı : " + sgk.getSGK_Matrah());       
        System.out.println("%20.5 : " + sgk.getYuzdeYirmiBucuk());
        System.out.println("%14 : " + sgk.getYuzdeOnDort());
        System.out.println("%2 : " + sgk.getYuzdeIki());
-       System.out.println("%1 : " + sgk.getYuzdeBir());
-       
-       
+       System.out.println("%1 : " + sgk.getYuzdeBir());      
        System.out.println("***********************************************");
        System.out.println("Toplam Tahakkuk : " + hakedis.getToplamTahakkuk());
        System.out.println("**************Kesintiler***********************");        
@@ -49,11 +45,6 @@ public class Main {
        System.out.println("Damgaaaaaaa Vergisi  : " + kesintiler.getDamgaVergisi());
        System.out.println("Kesintiler Toplamı : " + kesintiler.getKesintilerToplami());
        System.out.println("Net Ödenen : " + hakedis.netOdenen(kesintiler));
-       
-       
-       
-       
-
     }
 }
     
