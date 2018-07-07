feae1b9e80fd4c2f6b269f5962aa2d041760c3ac
Efe Özgür <efecuneytozgur@gmail.com>
2018-07-07 13:42:26 +0300

    laptop

--- src/Maas/anaForm.java
+++ src/Maas/anaForm.java
@@ -1,19 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Maas;
-
-/**
- *
- * @author Efe Özgür
- */
 public class anaForm extends javax.swing.JFrame {
 
-    /**
-     * Creates new form anaForm
-     */
     public anaForm() {
         initComponents();
     }
